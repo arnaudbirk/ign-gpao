e1ae9f2e0eb1fe9e51582f2c23e61fa1d05ad227
gregoire maillet <gregoire.maillet@ign.fr>
2020-05-18 10:59:00 +0200

    ajout gestion d erreur dans pgClient

--- api/middlewares/db/pgClient.js
+++ api/middlewares/db/pgClient.js
@@ -15,8 +15,13 @@ async function open(req, res, next) {
     port: process.env.PGPORT,
   });
   // Debut de la transaction
-  await req.pgPool.query('BEGIN');
-
+  await req.pgPool.query('BEGIN').catch((error) => {
+    req.error = {
+      msg: error.toString(),
+      code: 500,
+      function: 'pgClient open',
+    };
+  });
   next();
 }
 
@@ -24,10 +29,22 @@ async function close(req, res, next) {
   debug('close pg connection...');
   if (req.error) {
     debug('rollback');
-    await req.pgPool.query('ROLLBACK');
+    await req.pgPool.query('ROLLBACK').catch((error) => {
+      req.error = {
+        msg: error.toString(),
+        code: 500,
+        function: 'pgClient close rollback',
+      };
+    });
   } else {
     debug('commit');
-    await req.pgPool.query('COMMIT');
+    await req.pgPool.query('COMMIT').catch((error) => {
+      req.error = {
+        msg: error.toString(),
+        code: 500,
+        function: 'pgClient close commit',
+      };
+    });
   }
   req.pgPool.end();
 
